a8b4de47912b07bb724c9ca4b93954120ef06a4a
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-04-12 16:53:04 +0530

    Updates

---
+++ precache-manifest.d7457e2ce14beb3d1a67640440292172.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ac006a72ca23d5cebe414f01b9b6ab",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "129583c95d6fc71637d1",
+    "url": "/monsters-rolodex/static/css/main.1f930f3d.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "129583c95d6fc71637d1",
+    "url": "/monsters-rolodex/static/js/main.0c042f19.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
